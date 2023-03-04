97341c4b0463cab0f864f24b530661a47f889d43
JacobTTorres <77947250+JacobTTorres@users.noreply.github.com>
2023-03-04 13:09:39 -0600

    2 files modified

--- components/howWeWork/HowWeWork.js
+++ components/howWeWork/HowWeWork.js
@@ -4,23 +4,25 @@ import './howWeWork';
 export default function HowWeWork() {
   return (
     <div className="workContainer">
-      <h1>HOW WE WORK</h1>
+      <h1 className="workHeader">HOW WE WORK</h1>
       <div className="workContent">
         <div className="workLeft">
           <img
             src="https://img.pagecloud.com/OLMkCkXoRwmJM2lF-5vdSUL64jc=/0x341:355x689/248x0/filters:no_upscale()/rays-website-3454/Untitled-2-l3e21.png"
             alt=""
-            className="firstWork"
+            classname="workImg"
           />
         </div>
         <div className="workRight">
-          <h1 className="workLabel">1</h1>
-          <h2 className="workHeader">Market Research</h2>
-          <p className="workInfo">
-            We will research competitors and similar shows to find a GAP that
-            will allow us to properly position your show in the market and
-            across channels.
-          </p>
+          <div className="rightBox">
+            <h1 className="workLabel">1</h1>
+            <h2 className="workHeader">Market Research</h2>
+            <p className="workInfo">
+              We will research competitors and similar shows to find a GAP that
+              will allow us to properly position your show in the market and
+              across channels.
+            </p>
+          </div>
         </div>
       </div>
       <div className="workContent">
@@ -33,10 +35,13 @@ export default function HowWeWork() {
           </p>
         </div>
         <div className="workRight">
-          <img
-            src="https://img.pagecloud.com/9QxsXMmKWdEuBYgQBBGIMjQW0JA=/725x347:1046x662/253x0/filters:no_upscale()/rays-website-3454/Untitled-2-l3e21.png"
-            alt=""
-          />
+          <div className="rightBox">
+            <img
+              src="https://img.pagecloud.com/9QxsXMmKWdEuBYgQBBGIMjQW0JA=/725x347:1046x662/253x0/filters:no_upscale()/rays-website-3454/Untitled-2-l3e21.png"
+              alt=""
+              classname="workImg"
+            />
+          </div>
         </div>
       </div>
       <div className="workContent">
@@ -44,15 +49,18 @@ export default function HowWeWork() {
           <img
             src="https://img.pagecloud.com/F4OuwJGQkznheiWhAM8IAMUZ_NQ=/381x344:702x660/259x0/filters:no_upscale()/rays-website-3454/Untitled-2-l3e21.png"
             alt=""
+            classname="workImg"
           />
         </div>
         <div className="workRight">
-          <h1 className="workLabel">3</h1>
-          <h2 className="workHeader">Refine Your Podcast</h2>
-          <p className="workInfo">
-            We will clean up and polish each recording using state of the art
-            equipment to ensure that your podcast meets broadcast standards.
-          </p>
+          <div className="rightBox">
+            <h1 className="workLabel">3</h1>
+            <h2 className="workHeader">Refine Your Podcast</h2>
+            <p className="workInfo">
+              We will clean up and polish each recording using state of the art
+              equipment to ensure that your podcast meets broadcast standards.
+            </p>
+          </div>
         </div>
       </div>
       <div className="workContent">
